4572d7603fe051e241604c154da9e4cea197ab30
Apaar Seth <apaseth@publicisgroupe.net>
2019-11-24 23:56:34 +0530

    implemented pagination properly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MovieListService } from './movie-list.service';
-import { AngularWaitBarrier } from 'blocking-proxy/built/lib/angular_wait_barrier';
+import { map, tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
@@ -9,42 +11,86 @@ import { AngularWaitBarrier } from 'blocking-proxy/built/lib/angular_wait_barrie
 })
 export class AppComponent implements OnInit {
   title = 'movie-listing';
-  p:any;
+  search: boolean = false;
+  isLoading=false;
+  page: number = 1;
+  counter=0;
+  totalPage = 0;
   searchStr = "";
-  movieList = [];
+  movies = [];
+  config = {
+    id: "1",
+    totalItems: 0,
+    itemsPerPage: 20,
+    currentPage: 1
+  };
+  movieList: Observable<object[]>;
   pageOfItems: Array<any>;
   constructor(private movieService: MovieListService) {
+
   }
 
   ngOnInit() {
-    this.getMovies();
+    this.getMovies(this.page);
   }
 
-  getMovies() {
-    if (this.searchStr.length <=0) {
-      this.movieService.getMovies().subscribe((res) => {
-        this.movieList = res["results"];
-        console.log(res["results"]);
-      });
+  getMovies(page) {
+    if (this.searchStr.length <= 0) {
+      this.isLoading=true;
+      this.movieList = this.movieService.getMovies(page).pipe(tap(res => {
+        if (this.config.totalItems <= 0) {
+          this.config.totalItems = res["total_results"];
+        }
+        this.config.currentPage = page;
+        console.log("total page" + this.config.totalItems);
+        console.log(res["total_pages"]);
+      }), map(
+        (res) => {
+          this.isLoading=false;
+          console.log(res["results"])
+          return res["results"]
+        })
+      )
     }
   }
-
   getImage(posterPath) {
-    let imagePath = "https://image.tmdb.org/t/p/w500" + posterPath;
+    let imagePath = "/assets/images/image-not-available.png";
+    if (posterPath != null) {
+      imagePath = "https://image.tmdb.org/t/p/w500" + posterPath;
+    }
     return imagePath;
   }
-   searchMovies(){
+  searchMovies(page) {
     if (this.searchStr.length > 0) {
-      this.movieService.getMoviesBySeearch(this.searchStr).subscribe((res)=>{
-        this.movieList = res["results"];
-        console.log(res["results"]);
-      })
+      if(this.counter==0){
+          this.config.currentPage=1;
+          this.counter++;
+      }
+      this.isLoading=true;
+      this.search = true
+      this.movieList = this.movieService.getMoviesBySearch(this.searchStr,page).pipe(tap((res) => {
+        this.totalPage=res["total_results"];
+        this.config.totalItems = res["total_results"];
+        this.config.currentPage=page;
+      }), map(res => {
+        this.isLoading=false;
+        return res["results"];
+      }))
     }
-    else{
-      this.getMovies();
+    else {
+      this.config.totalItems = 0;
+      this.counter==0;
+      this.search=false;
+      this.getMovies(1);
     }
   }
-
-
+  changePage(event) {
+    console.log(event);
+    if (this.search)
+      this.searchMovies(event);
+    else
+      this.getMovies(event);
+    this.page = event;
+  }
 }
 
--- src/app/movie-list.service.ts
+++ src/app/movie-list.service.ts
@@ -9,14 +9,17 @@ export class MovieListService {
   constructor(private http: HttpClient) { }
 
   apiKey = "7040abd7cc4eda50581ff2e3f5d72f9e";
-  url = "https://api.themoviedb.org/4/list/1?page=2";
+  url = "https://api.themoviedb.org/4/list/1?";
 
-  getMovies() {
-    return this.http.get(this.url + "&api_key=" + this.apiKey);
+  getMovies(page:number) {
+    return this.http.get(this.url +"page="+page.toString()+ "&api_key=" + this.apiKey);
+  }
+  getMoviesByPage(page:number) {
+    return this.http.get(this.url +"page="+page.toString()+ "&api_key=" + this.apiKey);
   }
   
-  getMoviesBySeearch(movieName:string){
-    return this.http.get("https://api.themoviedb.org/3/search/movie?api_key=7040abd7cc4eda50581ff2e3f5d72f9e&language=en-US&query="+movieName+"&page=1&include_adult=false");
+  getMoviesBySearch(movieName:string,page:number){
+    return this.http.get("https://api.themoviedb.org/3/search/movie?api_key=7040abd7cc4eda50581ff2e3f5d72f9e&language=en-US&query="+movieName+"&page="+page+"&include_adult=false");
   }
 
 }
